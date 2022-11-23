a133004c08731d2375a584eb1a83672fb694db21
monik <Alex.uma2007@yandex.ru>
2022-11-23 19:00:14 +0500

    Data output from the database

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5 import uic
+from PyQt5 import uic, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QHeaderView
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel, QSqlQuery
 import sys
@@ -16,11 +16,24 @@ class MainWindow(QApplication):
         self.db.setDatabaseName('coffee.sqlite')
         self.db.open()
         self.table_view(self.tableView)
+        self.tableView.setColumnHidden(6, True)
         
     def table_view(self, view):
         model = QSqlTableModel(self, self.db)
-        model.setQuery(QSqlQuery(""""""))
-        
+        model.setQuery(QSqlQuery("""SELECT coffee_info.sort-name, roasting.roast-type,
+                                consistency.consistency-type, coffee_info.taste-description, coffee_info.price
+                                coffee_info.coffee-amount, coffee_info.id
+                                FROM coffee_info
+                                LEFT JOIN roasting ON coffee_info.roast = roasting.roast-id
+                                LEFT JOIN consistency ON coffee_info.consistency = consistensy.id"""))
+
+        model.setHeaderData(0, QtCore.Qt.Horizontal, 'Название сорта')
+        model.setHeaderData(1, QtCore.Qt.Horizontal, 'Степень обжарки')
+        model.setHeaderData(2, QtCore.Qt.Horizontal, 'Консистенция')
+        model.setHeaderData(3, QtCore.Qt.Horizontal, 'Описание вкуса')
+        model.setHeaderData(4, QtCore.Qt.Horizontal, 'Цена')
+        model.setHeaderData(5, QtCore.Qt.Horizontal, 'Объем упаковки, г.')
+
         model.select()
         view.setModel(model)
         view.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
